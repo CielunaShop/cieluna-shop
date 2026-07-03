7b178b950885cf7679d5c2962d56223d7766116b
Aaysha <quraishiaaysha05@gmail.com>
2026-07-03 11:23:36 +0530

    updated desktop view showcase hero

--- lib/ui/widgets/desktop/desktop_hero_showcase.dart
+++ lib/ui/widgets/desktop/desktop_hero_showcase.dart
@@ -8,36 +8,45 @@ class DesktopHeroShowcase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 520,
-      height: 620,
-
+      width: 430,
+      height: 400,
       child: Stack(
+        alignment: Alignment.center,
         clipBehavior: Clip.none,
         children: [
+          /// LEFT BACK CARD
           Positioned(
-            top: 80,
-            left: 30,
+            top: 54,
+            left: 8,
             child: _PlannerCard(
-              rotation: -.18,
+              rotation: -0.11,
               image: "assets/planners/fantasy/cover.png",
+              width: 180,
+              height: 260,
             ),
           ),
 
+          /// RIGHT BACK CARD
           Positioned(
-            top: 25,
-            right: 15,
+            top: 32,
+            right: 4,
             child: _PlannerCard(
-              rotation: .15,
-              image: "assets/planners/celestial/cover.png",
+              rotation: 0.10,
+              image: "assets/todos/cover.png",
+              width: 180,
+              height: 260,
             ),
           ),
 
+          /// MAIN FRONT CARD
           Positioned(
             bottom: 0,
-            left: 95,
+            left: 112,
             child: _PlannerCard(
               rotation: 0,
-              image: "assets/planners/notes/cover.png",
+              image: "assets/notes/cover.png",
+              width: 205,
+              height: 290,
               featured: true,
             ),
           ),
@@ -50,11 +59,15 @@ class DesktopHeroShowcase extends StatelessWidget {
 class _PlannerCard extends StatelessWidget {
   final String image;
   final double rotation;
+  final double width;
+  final double height;
   final bool featured;
 
   const _PlannerCard({
     required this.image,
     required this.rotation,
+    required this.width,
+    required this.height,
     this.featured = false,
   });
 
@@ -62,36 +75,29 @@ class _PlannerCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Transform.rotate(
       angle: rotation,
-
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-
-        width: featured ? 270 : 235,
-        height: featured ? 380 : 330,
-
+      child: Container(
+        width: width,
+        height: height,
+        padding: EdgeInsets.all(featured ? 10 : 8),
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(24),
-
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: border, width: 1),
           boxShadow: [
             BoxShadow(
               color: shadow,
-              blurRadius: 35,
-              offset: const Offset(0, 20),
+              blurRadius: featured ? 26 : 18,
+              offset: Offset(0, featured ? 14 : 10),
             ),
           ],
         ),
-
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(18),
-
-            child: Image.asset(
-              image,
-              fit: BoxFit.cover,
-            ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(14),
+          child: Image.asset(
+            image,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
           ),
         ),
       ),
